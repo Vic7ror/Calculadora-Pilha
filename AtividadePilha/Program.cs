cf48e5cf0720177f1f79dd825c54311e33bdc6ed
Victor Alexandre Ferreira Santos <125712570+Vic7ror@users.noreply.github.com>
2025-11-08 05:05:15 -0300

    Atividade de calculadora em pilha com c#

---
+++ AtividadePilha/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha p = new Pilha(100);
+
+            Console.WriteLine("(+) para somar       | ");
+            Console.WriteLine("(-) para diminuir    | ");
+            Console.WriteLine("(/) para dividir     | ");
+            Console.WriteLine("(*) para Multiplicar | ");
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (p.Cheia() == true)
+                {
+                    Console.WriteLine("Pilha Cheia!! Faça uma operação!");
+                }
+
+                Console.Write("\nDigite um n° ou valor: ");
+                string valor = Console.ReadLine();
+                if (valor == "+") {
+
+                    if (p.Vazia() == true)
+                    {
+                        Console.WriteLine("Pilha Vazia!!");
+                    }
+
+                    else
+                    {
+                        int n1 = p.Desempilhar();
+                        int n2 = p.Desempilhar();
+                        int n3;
+                        n3 = n2 + n1;
+                        p.Empilhar(n3);
+                        Console.WriteLine(n3);
+                    }
+                }
+                else if (valor == "-") {
+                    if (p.Vazia() == true)
+                    {
+                        Console.WriteLine("Pilha Vazia!!");
+                    }
+
+                    else
+                    {
+                        int n1 = p.Desempilhar();
+                        int n2 = p.Desempilhar();
+                        int n3;
+                        n3 = n2 - n1;
+                        p.Empilhar(n3);
+                        Console.WriteLine(n3);
+                    }
+                }
+                else if (valor == "*") {
+                    if (p.Vazia() == true)
+                    {
+                        Console.WriteLine("Pilha Vazia!!");
+                    }
+
+                        int n1 = p.Desempilhar();
+                        int n2 = p.Desempilhar();
+                        int n3;
+                        n3 = n2 * n1;
+                        p.Empilhar(n3);
+                    Console.WriteLine(n3);
+                }
+                else if (valor == "/") {
+                    if (p.Vazia() == true)
+                    {
+                        Console.WriteLine("Pilha Vazia!!");
+                    }
+
+                        int n1 = p.Desempilhar();
+                        int n2 = p.Desempilhar();
+                        int n3;
+                        n3 = n2 / n1;
+                        p.Empilhar(n3);
+                        Console.WriteLine(n3);
+                } 
+                else
+                {
+                    p.Empilhar(int.Parse(valor));
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# ATIVIDADE: CALCULADORA - PILHA(STACK)
+Escreva um programa que simule o funcionamento de uma calculadora científica, com memória de
+pilha. Qualquer número inteiro digitado pelo usuário deverá ser empilhado. Se o usuário digitar um dos
+quatro operadores matemáticos (+, -, *, /), o programa deverá realizar a operação desejada,
+desempilhando os dois últimos valores. O resultado da operação deverá ser empilhado e impresso na
+tela.
+
+---------------------------------------------------------------------------------------------------------
+Lenguage: C#
